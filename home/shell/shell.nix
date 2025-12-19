652142122306e5e41c28ef17d9c0edc0ec857a69
Franta Nautilus <comantelix36@gmail.com>
2025-12-19 14:48:40 +0100

    feat: ollama alias

--- home/shell/shell.nix
+++ home/shell/shell.nix
@@ -12,6 +12,7 @@
       e = "emacs";
       y = "yazi";
       v = "neovim";
+      ollama = "harbor ollama";
       #  k = "kubectl";
       #  urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
       #  urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
